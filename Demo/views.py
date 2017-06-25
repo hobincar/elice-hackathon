7c7e67ca64f5f298fe46dc81c1079b0ff32b7144
Hobin Ryu <hobincar@kaist.ac.kr>
2017-06-25 10:39:03 +0900

    test scatter chart

--- Demo/views.py
+++ Demo/views.py
@@ -73,22 +73,36 @@ def LR_4in(request):
     return render(request, "LR_4in.html", args)
 
 
+import time
+import datetime
+import random
 def test(request):
-    xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi",
-             "Lemon"]
-    ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
-    chartdata = {'x': xdata, 'y': ydata}
-    charttype = "pieChart"
-    chartcontainer = 'piechart_container'
+    """
+    scatterchart page
+    """
+    nb_element = 50
+    xdata = [i + random.randint(1, 10) for i in range(nb_element)] + [10] * 100
+    ydata1 = [i * random.randint(1, 10) for i in range(nb_element)] + [800] * 100
+    # xdata = [i + random.randint(1, 10) for i in range(nb_element)]
+    # ydata1 = [i * random.randint(1, 10) for i in range(nb_element)]
+    ydata2 = map(lambda x: x * 2, ydata1)
+    ydata3 = map(lambda x: x * 5, ydata1)
+
+    kwargs1 = {'shape': 'circle'}
+    kwargs2 = {'shape': 'cross'}
+    kwargs3 = {'shape': 'triangle-up'}
+
+    extra_serie1 = {"tooltip": {"y_start": "", "y_end": " balls"}}
+
+    chartdata = {
+        'x': xdata,
+        'name1': 'series 1', 'y1': ydata1, 'kwargs1': kwargs1, 'extra1': extra_serie1,
+        'name2': 'series 2', 'y2': ydata2, 'kwargs2': kwargs2, 'extra2': extra_serie1,
+        'name3': 'series 3', 'y3': ydata3, 'kwargs3': kwargs3, 'extra3': extra_serie1
+    }
+    charttype = "scatterChart"
     data = {
         'charttype': charttype,
         'chartdata': chartdata,
-        'chartcontainer': chartcontainer,
-        'extra': {
-            'x_is_date': False,
-            'x_axis_format': '',
-            'tag_script_js': True,
-            'jquery_on_ready': False,
-        }
     }
     return render_to_response('test.html', data)
